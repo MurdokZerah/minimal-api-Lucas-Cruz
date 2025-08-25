c9184518bac519a6bd7bb6073d1caebe6ff88076
Lucas Cruz <138832464+MurdokZerah@users.noreply.github.com>
2025-08-25 16:27:05 -0300

    Update 20231115020944_VeiculosMigration.cs

--- Api/Migrations/20231115020944_VeiculosMigration.cs
+++ Api/Migrations/20231115020944_VeiculosMigration.cs
@@ -5,18 +5,19 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace mininal_api.Migrations
 {
-    /// <inheritdoc />
     public partial class VeiculosMigration : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
             migrationBuilder.CreateTable(
                 name: "Veiculos",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                        .Annotation("MySql:ValueGenerationStrategy", MySql.EntityFrameworkCore.Metadata.MySqlValueGenerationStrategy.IdentityColumn),
                     Nome = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false)
                         .Annotation("MySql:CharSet", "utf8mb4"),
                     Marca = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
@@ -28,9 +29,18 @@ namespace mininal_api.Migrations
                     table.PrimaryKey("PK_Veiculos", x => x.Id);
                 })
                 .Annotation("MySql:CharSet", "utf8mb4");
+
+            // Seed inicial de Veiculos
+            migrationBuilder.InsertData(
+                table: "Veiculos",
+                columns: new[] { "Id", "Nome", "Marca", "Ano" },
+                values: new object[,]
+                {
+                    { 1, "Fiesta 2.0", "Ford", 2013 },
+                    { 2, "X6", "BMW", 2022 }
+                });
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
